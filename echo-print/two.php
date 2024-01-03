95fb153966c05c7d2ab0fe43b433984227b6dd2e
SHjalilo <aaaabdjalil@gmail.com>
2024-01-03 20:24:13 +0100

    add html with php

---
+++ echo-print/two.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title> <?php echo "Page 2"?> </title>
+</head>
+<body>
+
+<h1> <?php echo "hello from php - part 2"?> </h1>
+
+<h2> colors </h2>
+
+<ul>
+    <li> <?php echo "green"?> </li>
+    <li> <?php echo "gray"?> </li>
+    <li> <?php echo "black"?> </li>
+    <li> <?php echo "yellow"?> </li>
+    <li> <?php echo "pink"?> </li>    
+</ul>
+</body>
+</html>
