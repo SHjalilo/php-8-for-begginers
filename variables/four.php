d848be19ba02df140cf68bec8c727fc175c19dba
SHjalilo <aaaabdjalil@gmail.com>
2024-01-03 23:06:26 +0100

    how to write variables in php

---
+++ variables/four.php
@@ -0,0 +1,22 @@
+<?php
+
+echo "constant <br/>";
+
+define("TAX",43);
+
+
+echo TAX ;
+
+echo "<br/>";
+
+// 2 method to write a const
+
+const TVA = 21 ;
+
+echo TVA ;
+
+
+
+
+
+?>
---
+++ variables/one.php
@@ -0,0 +1,22 @@
+<?php
+
+$age = 30;
+
+$name = "saharaSOFT";
+
+echo $name." <br/>" ;
+
+echo $age ;
+
+echo "<br/>";
+
+$number3 = 3;
+
+$Youpi = "car";
+
+$_xyz = "xyz";
+
+
+echo " the ".$Youpi." number ".$number3." is ".$_xyz ;
+
+
