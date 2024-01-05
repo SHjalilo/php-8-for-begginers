b7c58a773df0ac47b40778f48f2de6c635ad139f
SHjalilo <aaaabdjalil@gmail.com>
2024-01-05 01:37:01 +0100

    how to create function in php

---
+++ functions/one.php
@@ -0,0 +1,17 @@
+<?php
+
+//declaration
+function plus($num1,$num2){
+    echo $num1 + $num2 ;
+}
+
+//calling
+plus(10,20);
+echo "<br/>";
+plus(880,20);
+echo "<br/>";
+plus(1090,60);
+echo "<br/>";
+plus(321,207);
+
+?>
---
+++ functions/two.php
@@ -0,0 +1,20 @@
+<?php
+// return function :
+
+function add($n1,$n2){
+    return $n1 + $n2 ;
+}
+
+// echo important in return case 
+
+echo add(5,3); //--> 8
+
+echo "<br/>";
+
+function fullName($first_name,$last_name){
+    return $first_name." ".$last_name;
+}
+
+//
+echo fullName("med","ali");
+?>
