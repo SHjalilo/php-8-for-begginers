d201c644308f185cadfde2c307958186021bdcf1
SHjalilo <aaaabdjalil@gmail.com>
2024-01-07 21:37:48 +0100

    add 2 fils .php explaine arrays

---
+++ arrays/three.php
@@ -0,0 +1,18 @@
+<?php
+
+$student = [
+    "name"=>"hamza chinwi",
+    "email"=>"hamza@gmail.com",
+    "phone"=>"0023451275",
+];
+
+echo $student["email"];
+echo "<br/>";
+
+echo $student["name"];
+echo "<br/>";
+
+echo $student["phone"];
+echo "<br/>";
+
+?>
---
+++ arrays/two.php
@@ -0,0 +1,12 @@
+<?php
+
+$colors = ["red","green","blue"];
+
+echo $colors[0];
+echo "<br/>";
+echo $colors[1];
+echo "<br/>";
+echo $colors[2];
+echo "<br/>";
+
+?>
