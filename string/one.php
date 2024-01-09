86093b29754ecb89b95b397cba494f4dc91e847f
SHjalilo <aaaabdjalil@gmail.com>
2024-01-09 22:38:55 +0100

    add string dir

---
+++ string/one.php
@@ -0,0 +1,15 @@
+<?php
+
+$company1 = 'sahara soft';
+$company2 = "sahara hard";
+
+echo "the company 1 name is : $company1"; // -> sahara soft
+echo "<br>";
+echo 'the company 2 name is : $company2'; // -> $company2
+
+// print " in my echo , using \ or ''
+echo "<br>";
+echo "hello \" world";
+echo "<br>";
+echo 'welcom in my php " cours';
+?>
