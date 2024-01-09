dfb99f8bb7c6725afe103dc1c441b027978975b1
SHjalilo <aaaabdjalil@gmail.com>
2024-01-10 00:18:44 +0100

    add function to string dir ?

--- string/stringFunctions.php
+++ string/stringFunctions.php
@@ -4,7 +4,26 @@ $mytext = "salamo alaykom everybody !";
 
 // string like array in php 
 echo $mytext[2]; // => l
+echo "<br>";
 
-//
+// linitation in string
+
+echo substr($mytext,15,4); // => ever
+echo "<br>";
+
+// replace 
+
+echo str_replace("everybody","brothers",$mytext); 
+echo "<br>";
+
+// length
+
+echo strlen($mytext);
+echo "<br>";
+
+// conitain or find element in string
+
+$result = str_contains($mytext,"alaykom") ? "yes" : "no";
+echo "the word exists ? => : $result";
 
 ?>
