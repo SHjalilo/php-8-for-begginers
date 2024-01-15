8183a63d67e41087299a971228db85d5eef4dcb7
SHjalilo <aaaabdjalil@gmail.com>
2024-01-15 14:03:32 +0100

    condition : if - else

---
+++ conditions/one.php
@@ -0,0 +1,27 @@
+<?php
+
+// if - else 
+$num = 6 ;
+
+if($num < 8)
+    echo "negative number ";
+elseif($num == 0)
+    echo "number is null";
+else 
+    echo "positive number ";
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+?>
