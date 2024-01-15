00721e6194578a7dd27ba0def19bcb6224b510f9
SHjalilo <aaaabdjalil@gmail.com>
2024-01-15 14:30:05 +0100

    loops ; for / while

---
+++ loops/one.php
@@ -0,0 +1,33 @@
+<?php
+
+// for loop
+
+for($i=0;$i<9;$i++){
+    echo $i . '<br>';
+}
+
+
+// while
+
+echo '<br>';
+$t = 5;
+
+while($t < 13){
+
+    echo "hello world ";
+    $t++;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+?>
