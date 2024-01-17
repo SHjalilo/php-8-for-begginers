a23f1599dbc9cc839720258535ce01d8ba5eedc5
SHjalilo <aaaabdjalil@gmail.com>
2024-01-17 20:10:16 +0100

    date : name of day

--- date/one.php
+++ date/one.php
@@ -3,7 +3,21 @@
 //
 echo date('d/m/y');
 
+// write the name of day and month
+
+echo "<br>";
+echo date('D/M/Y');
 
 
+// write the full name of day 
+
+echo "<br>";
+echo date('l D/M/Y');
+
+
+// write the hours
+
+echo "<br>";
+echo date('D/M/Y');
 
 ?>
